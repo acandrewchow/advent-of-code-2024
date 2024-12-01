c888df3bff88984cc849c57acb831e1a3bedc1f4
Andrew Chow <ac.andrewchow@gmail.com>
2024-12-01 00:59:23 -0500

    aoc day 1

---
+++ day1/day1.py
@@ -0,0 +1,31 @@
+# day 1
+def read_file():
+
+    with open("day1.txt", "r") as file:
+        list1 = []
+        list2 = []
+        
+        for line in file:
+            num1, num2 = map(int, line.split())
+
+            list1.append(num1)
+            list2.append(num2)
+
+    return list1, list2
+
+def solution(list1, list2):
+    total_distance = 0
+
+    list1.sort()
+    list2.sort()
+
+    for i in range(len(list1)):
+        result = abs(list1[i] - list2[i])
+
+        total_distance += result
+
+    return total_distance
+
+list1, list2 = read_file()
+
+print(solution(list1, list2))
